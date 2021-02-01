0fcf2594931265dc5cd66bd806ad9d062d5e974c
afarhan39 <amir.farhan130794@gmail.com>
2021-02-01 08:31:42 +0800

    - added voteAverageImg, voteAverageAndCount and genre - added NestedScrollView to detail - removed launchSingleTop when actListToDetail - added color for day and night resource

--- app/src/main/java/my/farhan/movie/util/BindingAdapters.kt
+++ app/src/main/java/my/farhan/movie/util/BindingAdapters.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package my.farhan.movie.util
 
 import android.view.View
@@ -24,13 +8,13 @@ import com.google.android.material.textview.MaterialTextView
 import my.farhan.movie.R
 
 object BindingAdapters {
-    @BindingAdapter(value = ["app:hideIfEmpty"])
+    @BindingAdapter(value = ["hideIfEmpty"])
     @JvmStatic
     fun hideIfEmpty(view: View, isEmpty: Boolean) {
         view.visibility = if (isEmpty) View.GONE else View.VISIBLE
     }
 
-    @BindingAdapter("app:showIfEmpty")
+    @BindingAdapter("showIfEmpty")
     @JvmStatic
     fun showIfEmpty(view: View, isEmpty: Boolean) {
         view.visibility = if (isEmpty) View.VISIBLE else View.GONE
@@ -52,5 +36,35 @@ object BindingAdapters {
             view.text = view.context.getString(R.string.unavailable)
         else
             view.text = view.context.getString(R.string.duration, runTime)
+    }
+
+    @BindingAdapter("voteAverageImg")
+    @JvmStatic
+    fun voteAverageImg(view: ImageView, voteAverage: Double) {
+        when {
+            voteAverage == 0.0 -> view.setImageResource(R.drawable.ic_star_0)
+            voteAverage < 1 -> view.setImageResource(R.drawable.ic_star_0_5)
+            voteAverage < 2 -> view.setImageResource(R.drawable.ic_star_1)
+            voteAverage < 3 -> view.setImageResource(R.drawable.ic_star_1_5)
+            voteAverage < 4 -> view.setImageResource(R.drawable.ic_star_2)
+            voteAverage < 5 -> view.setImageResource(R.drawable.ic_star_2_5)
+            voteAverage < 6 -> view.setImageResource(R.drawable.ic_star_3)
+            voteAverage < 7 -> view.setImageResource(R.drawable.ic_star_3_5)
+            voteAverage < 8 -> view.setImageResource(R.drawable.ic_star_4)
+            voteAverage < 9 -> view.setImageResource(R.drawable.ic_star_4_5)
+            voteAverage < 10 -> view.setImageResource(R.drawable.ic_star_5)
+        }
+    }
+
+    @BindingAdapter(value = ["voteAverage", "voteCount"])
+    @JvmStatic
+    fun voteAverageAndCount(view: MaterialTextView, voteAverage: Double, voteCount: Int) {
+        view.text = "${voteAverage.round(1)}($voteCount)"
+    }
+
+    @BindingAdapter("genre")
+    @JvmStatic
+    fun genre(view: MaterialTextView, genre: List<String>) {
+        view.text = genre.joinToString(separator = ", ") { it }
     }
 }
--- app/src/main/java/my/farhan/movie/util/CommonExts.kt
+++ app/src/main/java/my/farhan/movie/util/CommonExts.kt
@@ -5,3 +5,9 @@ val Any.TAG: String
         val tag = javaClass.simpleName
         return if (tag.length <= 23) tag else tag.substring(0, 23)
     }
+
+fun Double.round(decimals: Int): Double {
+    var multiplier = 1.0
+    repeat(decimals) { multiplier *= 10 }
+    return kotlin.math.round(this * multiplier) / multiplier
+}
