6ec3f0741cf6395ce8073eaf1026a9943992f4f8
afarhan39 <amir.farhan130794@gmail.com>
2021-02-01 20:46:52 +0800

    - ditch fragment, now use activity - much smoother interaction, no longer weird issue recreateView

---
+++ app/src/main/java/my/farhan/favy/ui/detail/MovieDetailActivity.kt
@@ -0,0 +1,46 @@
+package my.farhan.favy.ui.detail
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.liaoinstan.springview.container.AutoFooter
+import com.liaoinstan.springview.widget.SpringView
+import my.farhan.favy.R
+import my.farhan.favy.data.SortMethod
+import my.farhan.favy.databinding.ActivityMovieDetailBinding
+import my.farhan.favy.databinding.ActivityMovieListBinding
+import my.farhan.favy.databinding.FragmentMovieDetailBinding
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class MovieDetailActivity : AppCompatActivity() {
+    private val movieDetailVM by viewModel<MovieDetailVM>()
+    private lateinit var bv: ActivityMovieDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bv = DataBindingUtil.setContentView(this, R.layout.activity_movie_detail)
+        bv.vm = movieDetailVM
+        bv.activity = this
+        bv.lifecycleOwner = this
+    }
+
+    fun bookNow() {
+        val url = "https://www.cathaycineplexes.com.sg/"
+        val customTabBuilder = CustomTabsIntent.Builder()
+        val customTabColorBuilder = CustomTabColorSchemeParams.Builder()
+        customTabColorBuilder.setToolbarColor(resources.getColor(R.color.purple_200, null))
+        customTabColorBuilder.setSecondaryToolbarColor(resources.getColor(R.color.teal_200, null))
+        customTabColorBuilder.setNavigationBarColor(resources.getColor(R.color.purple_700, null))
+        customTabColorBuilder.setNavigationBarDividerColor(resources.getColor(R.color.black, null))
+        customTabBuilder.setDefaultColorSchemeParams(customTabColorBuilder.build())
+        customTabBuilder.build().launchUrl(this, Uri.parse(url))
+    }
+}
---
+++ app/src/main/java/my/farhan/favy/ui/list/MovieListActivity.kt
@@ -0,0 +1,101 @@
+package my.farhan.favy.ui.list
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.liaoinstan.springview.container.AutoFooter
+import com.liaoinstan.springview.widget.SpringView
+import my.farhan.favy.R
+import my.farhan.favy.data.SortMethod
+import my.farhan.favy.data.network.Status
+import my.farhan.favy.databinding.ActivityMovieListBinding
+import my.farhan.favy.ui.detail.MovieDetailActivity
+import my.farhan.favy.util.SpacesItemDecoration
+import my.farhan.favy.util.TAG
+import my.farhan.favy.util.observeOnceNonNull
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class MovieListActivity : AppCompatActivity(), MoviesAdapter.Listener {
+    private val movieListVM by viewModel<MovieListVM>()
+    private lateinit var bv: ActivityMovieListBinding
+    private lateinit var moviesAdapter: MoviesAdapter
+    private lateinit var sortPopup: PopupMenu
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bv = DataBindingUtil.setContentView(this, R.layout.activity_movie_list)
+        bv.vm = movieListVM
+        bv.activity = this
+        bv.lifecycleOwner = this
+
+//        movieListVM.sortBy(SortMethod.ReleaseDate)
+        initPopup()
+        setAdapter()
+        bv.svContainer.setListener(object : SpringView.OnFreshListener {
+            override fun onRefresh() {
+                movieListVM.onRefreshMovies()
+            }
+
+            override fun onLoadmore() {
+                movieListVM.onLoadMoreMovies()
+            }
+        })
+        bv.svContainer.footer = AutoFooter()
+    }
+
+    override fun onClickMovie(movieId: Int) {
+        movieListVM.getMovieDetails(movieId)
+        movieListVM.selectedMovie.observeOnceNonNull(this, {
+            startActivity(Intent(this, MovieDetailActivity::class.java))
+        })
+    }
+
+    private fun initPopup() {
+        sortPopup = PopupMenu(this, bv.clSort)
+        for (item in movieListVM.sortOptionList)
+            sortPopup.menu.add(item)
+
+        sortPopup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            val sortMethod = SortMethod.fromLabel(menuItem.toString())
+            movieListVM.sortBy(sortMethod)
+            true
+        }
+    }
+
+    fun showSortPopup() {
+        sortPopup.show()
+    }
+
+    private fun setAdapter() {
+        moviesAdapter = MoviesAdapter(this)
+        bv.rvMovies.layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+        bv.rvMovies.adapter = moviesAdapter
+        val decoration = SpacesItemDecoration(16)
+        bv.rvMovies.itemAnimator = DefaultItemAnimator()
+        bv.rvMovies.addItemDecoration(decoration)
+        movieListVM.movies.observe(this, {
+            if (it.isNotEmpty() && it != null) {
+                moviesAdapter.submitList(it)
+            }
+        })
+        movieListVM.apiEvent.observe(this, {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    bv.svContainer.onFinishFreshAndLoad()
+                }
+                Status.ERROR -> {
+                    bv.svContainer.onFinishFreshAndLoad()
+                }
+                Status.LOADING -> {
+                }
+            }
+        })
+    }
+}
